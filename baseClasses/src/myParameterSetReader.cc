2cf3d95e0c5d296cd24472a9950b3897fbeb4ae7
Patrick Bryant <patrickbryant@me.com>
2020-04-07 08:40:47 -0500

    remove my python parsing hack

--- baseClasses/src/myParameterSetReader.cc
+++
@@ -1,7 +0,0 @@
-#include "nTupleAnalysis/baseClasses/interface/myParameterSetReader.h"
-#include "FWCore/PythonParameterSet/interface/PythonProcessDesc.h"
-#include "nTupleAnalysis/baseClasses/interface/myMakeParameterSets.h"
-
-std::unique_ptr<edm::ParameterSet> edm::readPSetsFrom(std::string const& fileOrString, int argc, char* argv[]) {
-  return edm::boost_python::readPSetsFrom(fileOrString, argc, argv);
-}
