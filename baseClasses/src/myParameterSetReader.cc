49c170da702e2fcd837597148420664dd8c883a7
John Alison <johnalison@cmu.edu>
2020-04-29 19:37:43 +0200

    Adding .cc

---
+++ baseClasses/src/myParameterSetReader.cc
@@ -0,0 +1,14 @@
+// Uncomment for SLC6
+//#define NTUPLEANALYSIS_SLC6 1
+
+#if defined NTUPLEANALYSIS_SLC6
+
+#include "nTupleAnalysis/baseClasses/interface/myParameterSetReader.h"
+#include "FWCore/PythonParameterSet/interface/PythonProcessDesc.h"
+#include "nTupleAnalysis/baseClasses/interface/myMakeParameterSets.h"
+
+std::unique_ptr<edm::ParameterSet> edm::readPSetsFrom(std::string const& fileOrString, int argc, char* argv[]) {
+  return edm::boost_python::readPSetsFrom(fileOrString, argc, argv);
+}
+
+#endif
