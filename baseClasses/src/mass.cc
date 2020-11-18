b9a998f7597f4ae400a552a9b585a6849ed39d09
Wesley Terrill <terrill.37@osu.edu>
2020-11-18 17:11:34 +0100

    changed mass range

--- baseClasses/src/mass.cc
+++ baseClasses/src/mass.cc
@@ -7,7 +7,7 @@ mass::mass(std::string name, fwlite::TFileService& fs) {
 
   dir = fs.mkdir(name);
   DeepCSV = dir.make<TH1F>("deepCSV", (name+"/deepCSV; ;Entries").c_str(), 120, -0.2, 1.2);
-  invariant_mass    = dir.make<TH1F>("mass", (name+"/mass; ;Entries").c_str(), 500, 0, 100);
+  invariant_mass    = dir.make<TH1F>("mass", (name+"/mass; ;Entries").c_str(), 100, 0, 1000);
   
  // cutL1  = dir.make<TH1F>("cutL1", (name+"/test; ;Entries").c_str(), 64, 0, 64);
 }
