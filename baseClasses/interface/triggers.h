47e881f8d37a38e0af42fdae23620a75dbbb0ef2
Wesley Terrill <terrill.37@osu.edu>
2020-11-23 16:56:44 +0100

    add pt_all and pt_cut

--- baseClasses/interface/triggers.h
+++ baseClasses/interface/triggers.h
@@ -19,6 +19,8 @@ namespace nTupleAnalysis {
     triggers(std::string, fwlite::TFileService&);
     void Fill(long unsigned int);
     void AddCut(std::string cut);
+    void Fillpt_all(float);
+    void Fillpt_cut(float);
     ~triggers(); 
 
   };
--- baseClasses/src/triggers.cc
+++ baseClasses/src/triggers.cc
@@ -7,6 +7,10 @@ triggers::triggers(std::string name, fwlite::TFileService& fs) {
 
   dir = fs.mkdir(name);
   full = dir.make<TH1F>("full", (name+"/test; ;Entries").c_str(), 64, 0, 64);
+  pt_all = dir.make<TH1F>("pt_all", (name+"/test; Entries").c_str(), 100, 0, 1000);
+  pt_cut = dir.make<TH1F>("pt_cut", (name+"/test; Entries").c_str(), 100, 0, 1000);
+ 
+ 
  // cutL1  = dir.make<TH1F>("cutL1", (name+"/test; ;Entries").c_str(), 64, 0, 64);
 }
 
@@ -24,6 +28,13 @@ void triggers::AddCut(std::string cut){
   return;
 }
 
+void triggers::Fillpt_all(float pt){
+  pt_all->Fill(pt);
+}
+
+void triggers::Fillpt_cut(float pt){
+  pt_cut->Fill(pt);
+}
 
 triggers::~triggers(){} 
 
